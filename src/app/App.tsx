6a55baf9e89cfe023e95172f067030eece9742ef
Moshniaga Artem <artem.m89@mail.ru>
2024-10-01 12:12:23 +0300

    feat: add base css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
     <div className='bg-slate-600'>
-      <h1 className='text-3xl font-bold underline'> Hello world!</h1>
+      <h1 className='text-3xl font-bold underline'>GITHUB REPO</h1>
     </div>
   );
 };
